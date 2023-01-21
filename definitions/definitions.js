4b337dbcd6867ab87bce5a5bdcdbe74f83c945a9
Keisuke Emura <keyem4251@gmail.com>
2023-01-21 13:56:23 +0000

    js example 2

--- definitions/definitions.js
+++ definitions/definitions.js
@@ -1,8 +1,16 @@
 // publish argument
-  publish("js_publish_example", {
-    type: "table",
-    dependencies: ["quickstart-source"],
-    assertions: {
-      rowConditions: ["test >= 1"]
-    }
-  }).query(ctx => "SELECT 1 AS test");
+publish("js_publish_example", {
+  type: "table",
+  dependencies: ["quickstart-source"],
+  assertions: {
+    rowConditions: ["test >= 1"]
+  }
+}).query(ctx => "SELECT 1 AS test");
+
+publish("js_publish_example_2").config({
+  type: "table",
+  dependencies: ["quickstart-source"],
+  assertions: {
+    rowConditions: ["test >= 1"]
+  }
+}).query(ctx => "SELECT 1 AS test");
