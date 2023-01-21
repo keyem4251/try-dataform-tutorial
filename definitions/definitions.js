a3bb886d887f60eb0ae6ae1d4c3c9bdfc6759c76
Keisuke Emura <keyem4251@gmail.com>
2023-01-21 13:57:08 +0000

    add message

--- definitions/definitions.js
+++ definitions/definitions.js
@@ -7,6 +7,7 @@ publish("js_publish_example", {
   }
 }).query(ctx => "SELECT 1 AS test");
 
+// set config 
 publish("js_publish_example_2").config({
   type: "table",
   dependencies: ["quickstart-source"],
