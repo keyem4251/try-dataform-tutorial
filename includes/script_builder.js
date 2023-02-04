5b404a210e850f4b03af100d6227c092c4e0cb91
Keisuke Emura <keyem4251@gmail.com>
2023-02-04 02:21:18 +0000

    mod compile error

--- includes/script_builder.js
+++ includes/script_builder.js
@@ -1,7 +1,7 @@
 function renderScript(table, dimensions, metrics) {
     return `
         SELECT
-        ${dimensions.map((field) => `${field} AS ${field}`).join(",\\n")},
+        ${dimensions.map((field) => `${field} AS ${field}`).join(",")},
         ${metrics.map((field) => `SUM(${field}) AS ${field}`).join(",\\n")},
         FROM ${table}
         GROUP BY ${dimensions.map((field, i) => `${i + 1}`).join(", ")}
