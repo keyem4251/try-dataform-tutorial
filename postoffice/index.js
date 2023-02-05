dbeb10515399e690c697d0284cabb00e18f93892
Keisuke Emura <keyem4251@gmail.com>
2023-02-05 07:09:33 +0000

    use pakcage function

--- postoffice/index.js
+++ postoffice/index.js
@@ -4,7 +4,7 @@ function getDomain(email) {
     let cleanEmail = `trim(${email})`
     const domain = `substr(${cleanEmail}, strpos(${cleanEmail}, '@') + 1)`;
     return `case
-        when ${domain} in ${common.GENERIC_DOMAINS} then ${cleanEmail}
+        when ${domain} in ${GENERIC_DOMAINS} then ${cleanEmail}
         when ${domain} in "othermailbox.com" then "other.com"
         when ${domain} in "mailbox.com" then "mailbox.global"
         else ${domain}
